a2be3ab62f2fa2a9a786cf9d6865a7ec994f0681
Yashwanth <100037024+yashwanth-smarty@users.noreply.github.com>
2022-04-23 19:26:41 +0530

    Delete SpeakHello.js

--- SpeakHello.js
+++
@@ -1,8 +0,0 @@
-//SpeakHello
-(function (window) {
-  var helloSpeaker = {};
-  helloSpeaker.speak1=function(name1){
-    console.log("Hello " + name1);
-  }
-  window.helloSpeaker=helloSpeaker;
-})(window);
